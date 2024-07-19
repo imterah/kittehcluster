9475570b186ce882a54086b1262249b22aa6d7cb
greysoh <me@greysoh.dev>
2024-07-19 16:47:28 -0400

    fix: FINALLY fix all the k3s issues!
    With a hack, of course. :3

--- nixinfra/commons.agent.nix
+++ nixinfra/commons.agent.nix
@@ -8,16 +8,28 @@ in {
 
   systemd.services.k3s = {
     enable = true;
-    description = "(manual) k3s service";
-
-    path = [
-      pkgs.k3s
-    ];
+    description = "KittehCluster's modified k3s service";
 
     serviceConfig = {
       Type = "simple";
       ExecStart = pkgs.writeShellScript "k3s-hack" ''
-        k3s agent --token ${k3s_token} --server https://kitteh-node-1-k3s-server:6443
+        if [ ! -d "/tmp/k3shack" ]; then
+          # Manually recreate the symlinks. Don't @ me.
+          ln -s ${pkgs.k3s}/bin/k3s /tmp/k3shack/containerd
+          ln -s ${pkgs.k3s}/bin/k3s /tmp/k3shack/crictl
+          ln -s ${pkgs.k3s}/bin/k3s /tmp/k3shack/ctr
+          ln -s ${pkgs.k3s}/bin/k3s /tmp/k3shack/k3s-agent
+          ln -s ${pkgs.k3s}/bin/k3s /tmp/k3shack/k3s-certificate
+          ln -s ${pkgs.k3s}/bin/k3s /tmp/k3shack/k3s-completion
+          ln -s ${pkgs.k3s}/bin/k3s /tmp/k3shack/k3s-etcd-snapshot
+          ln -s ${pkgs.k3s}/bin/k3s /tmp/k3shack/k3s-secrets-encrypt
+          ln -s ${pkgs.k3s}/bin/k3s /tmp/k3shack/k3s-server
+          ln -s ${pkgs.k3s}/bin/k3s /tmp/k3shack/k3s-token
+          ln -s ${pkgs.k3s}/bin/k3s /tmp/k3shack/kubectl
+        fi
+
+        export PATH=$PATH:/tmp/k3shack
+        ${pkgs.k3s}/bin/k3s agent --token ${k3s_token} --server https://kitteh-node-1-k3s-server:6443
       '';
     };
   };
